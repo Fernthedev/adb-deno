a06b0e0b8916bb1fa13bd10208d16bf76d3d26cf
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-10-12 09:58:41 -0400

    Use npm import

--- deps.ts
+++ deps.ts
@@ -1,4 +1,3 @@
 export * as path from "https://deno.land/std@0.149.0/path/mod.ts";
 
-// @deno-types="https://cdn.skypack.dev/fflate/lib/index.d.ts"
-export * as fflate from "https://cdn.skypack.dev/fflate?min";
+export * as fflate from "npm:fflate@^0.8.0";
